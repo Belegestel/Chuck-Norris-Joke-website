614d5885722d1a9ce84bedb1d943cb0b54b27472
Wojciech Dudziński <wojdud.pl@gmail.com>
2026-04-30 21:11:17 +0200

    Jokes controller fixes

--- src/jokes/jokes.controller.ts
+++ src/jokes/jokes.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Query, UseGuards, Param, Body, Request } from '@nestjs/common';
 import { JokesService } from './jokes.service';
 import { AuthGuard } from '../auth/auth.guard';
 
@@ -18,5 +18,25 @@ export class JokesController {
   @Get('categories')
   getCategories() {
     return this.jokesService.getCategories();
+  }
+
+  @Post('save')
+  async save(@Body('value') jokeText: string, @Request() req) {
+    return this.jokesService.saveJoke(jokeText, req.user.sub);
+  }
+
+  @Get('my-jokes')
+  async getMyJokes(@Request() req) {
+    return this.jokesService.getUserJokes(req.user.sub);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: number, @Request() req) {
+    return this.jokesService.deleteJoke(id, req.user.sub);
+  }
+
+  @Post('add')
+  async addJoke(@Body('value') value: string, @Request() req) {
+    return this.jokesService.saveJoke(value, req.user.sub);
   }
 }
