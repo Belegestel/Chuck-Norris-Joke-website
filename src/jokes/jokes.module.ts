f5d46476cfee934a64d3eb2128b4d181f2825d98
Wojciech Dudziński <wojdud.pl@gmail.com>
2026-05-01 14:08:22 +0200

    Fixed dependencies

--- src/jokes/jokes.module.ts
+++ src/jokes/jokes.module.ts
@@ -4,11 +4,15 @@ import { JokesService } from './jokes.service';
 import { JokesController } from './jokes.controller';
 import { HttpModule } from '@nestjs/axios';
 import { Joke } from './joke.entity';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     HttpModule,
     TypeOrmModule.forFeature([Joke]),
+    ConfigModule,
+    AuthModule,
   ],
   providers: [JokesService],
   controllers: [JokesController]
